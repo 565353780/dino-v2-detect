057c03e3d2651420e2c72bfd51452b436fc4a89b
chLi <565353780@qq.com>
2024-12-15 01:59:18 +0800

    successful load dinov2 from model code

--- README.md
+++ README.md
@@ -5,7 +5,7 @@
 ```bash
 https://dl.fbaipublicfiles.com/dinov2/dinov2_vitg14/dinov2_vitg14_reg4_pretrain.pth
 ->
-$HOME/.cache/torch/hub/checkpoints/dinov2_vitg14_reg4_pretrain.pth
+dinov2_vitg14_reg4_pretrain.pth
 ```
 
 ## Setup
--- dino_v2_detect/Module/detector.py
+++ dino_v2_detect/Module/detector.py
@@ -14,8 +14,6 @@ class Detector(object):
         # self.dtype = torch.bfloat16 if torch.cuda.is_bf16_supported() else torch.float16
         self.dtype = torch.float16
 
-        model = torch.hub.load('facebookresearch/dinov2', 'dinov2_vitg14_reg')
-
         self.model = vit_giant2(
             patch_size=14,
             num_register_tokens=4,
@@ -24,6 +22,7 @@ class Detector(object):
             block_chunks=0,
             interpolate_antialias=True,
             interpolate_offset=0.0,
+            init_values=1.0,
         )
         self.model = self.model.to(self.device, dtype=self.dtype)
         self.model.eval()
