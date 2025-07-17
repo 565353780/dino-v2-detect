1ca8cdac8cec19a6a90ba6327eb3653a8cacd6de
chLi <565353780@qq.com>
2025-07-17 16:00:54 +0800

    update model path

--- dino_v2_detect/Demo/detector.py
+++ dino_v2_detect/Demo/detector.py
@@ -1,24 +1,34 @@
+import os
 import torch
 from tqdm import trange
 
 from dino_v2_detect.Module.detector import Detector
 
+
 def demo():
-    model_type = 'base'
-    model_file_path = '/home/chli/chLi/Model/DINOv2/dinov2_vitb14_reg4_pretrain.pth'
-    dtype = 'auto'
-    device = 'cuda'
-    image_file_path = '/home/chli/chLi2/Dataset/CapturedImage/ShapeNet/02691156/10155655850468db78d106ce0a280f87/y_5_x_3.png'
+    model_type = "large"
+    model_file_path = (
+        os.environ["HOME"] + "/chLi/Model/DINOv2/dinov2_vitb14_reg4_pretrain.pth"
+    )
+    dtype = "auto"
+    device = "cuda:0"
+    image_file_path = "/home/chli/chLi2/Dataset/CapturedImage/ShapeNet/02691156/10155655850468db78d106ce0a280f87/y_5_x_3.png"
 
     detector = Detector(model_type, model_file_path, dtype, device)
 
     for _ in trange(100):
-        dino_feature = detector.detect(torch.randn([3, 3, 518, 518], dtype=torch.float32, device='cpu'))
+        dino_feature = detector.detect(
+            torch.randn([3, 3, 518, 518], dtype=torch.float32, device="cpu")
+        )
+
+    print("dino_feature:")
+    print(dino_feature)
+    print(dino_feature.shape)
 
     for _ in trange(100):
         dino_feature = detector.detectFile(image_file_path)
 
-    print('dino_feature:')
+    print("dino_feature:")
     print(dino_feature)
     print(dino_feature.shape)
     return True
